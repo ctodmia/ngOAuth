c1501a97ae35fadde9a3d173065a243e88e0fb65
ctodmia <ctodmia@gmail.com>
2015-12-06 16:18:53 -0800

    profile and signup page properly routed.

---
+++ client/app.js
@@ -0,0 +1,38 @@
+angular.module('myapp', [
+	'ui.router',
+	'ui.bootstrap', 
+	'ui.bootstrap',
+	'satellizer',
+])
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider, $authProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'signup/signup.html',
+			controller: 'SignupController as signup'
+			})
+			.state('profile', {
+			url: '/profile',
+			templateUrl: 'profile/profileView.html',
+			controller: 'ProfileController as profile'
+			})
+})
+.controller('SignupController', function SignupController() {
+
+	var signup = this;
+
+	signup.greeting = "First"
+
+})
+
+.controller('Controller', function SignupController() {
+
+	var signup = this;
+
+	signup.greeting = "First"
+
+})
